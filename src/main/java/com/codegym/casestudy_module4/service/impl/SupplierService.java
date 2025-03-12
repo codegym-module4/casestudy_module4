078e9cc1af90784870590c7af9034269bf20901f
LinhChi2511 <chidao2511@gmail.com>
2025-03-12 14:03:13 +0700

    Change

--- src/main/java/com/codegym/casestudy_module4/service/impl/SupplierService.java
+++ src/main/java/com/codegym/casestudy_module4/service/impl/SupplierService.java
@@ -2,22 +2,16 @@ package com.codegym.casestudy_module4.service.impl;
 
 import com.codegym.casestudy_module4.entity.Customer;
 import com.codegym.casestudy_module4.entity.Supplier;
-<<<<<<< HEAD
 import com.codegym.casestudy_module4.repository.ISupplierRepository;
 import com.codegym.casestudy_module4.service.ICustomerService;
 import com.codegym.casestudy_module4.service.ISupplierService;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
-import com.codegym.casestudy_module4.service.ICustomerService;
-import com.codegym.casestudy_module4.service.ISupplierService;
->>>>>>> 732731e2ebe6e4f8a01f855637362f4172ca8e92
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class SupplierService implements ISupplierService {
-<<<<<<< HEAD
 
     @Autowired
     private ISupplierRepository supplierRepository;
@@ -26,34 +20,20 @@ public class SupplierService implements ISupplierService {
     public List<Supplier> getAll() {
         List<Supplier> suppliers = supplierRepository.findAll();
         return suppliers;
-=======
-
-    @Override
-    public List<Supplier> getAll() {
-        return List.of();
->>>>>>> 732731e2ebe6e4f8a01f855637362f4172ca8e92
     }
 
     @Override
     public void save(Supplier s) {
-<<<<<<< HEAD
         supplierRepository.save(s);
-=======
-
->>>>>>> 732731e2ebe6e4f8a01f855637362f4172ca8e92
     }
 
     @Override
     public void update(long id, Supplier s) {
-<<<<<<< HEAD
         Supplier supplier = supplierRepository.findById(id).orElse(null);
         if (supplier != null){
             s.setId(id);
             supplierRepository.save(s);
         }
-=======
-
->>>>>>> 732731e2ebe6e4f8a01f855637362f4172ca8e92
     }
 
     @Override
@@ -63,19 +43,11 @@ public class SupplierService implements ISupplierService {
 
     @Override
     public Supplier findById(long id) {
-<<<<<<< HEAD
         return supplierRepository.findById(id).orElse(null);
-=======
-        return null;
->>>>>>> 732731e2ebe6e4f8a01f855637362f4172ca8e92
     }
 
     @Override
     public List<Supplier> findByName(String name) {
-<<<<<<< HEAD
         return supplierRepository.findAllByNameContaining(name);
-=======
-        return List.of();
->>>>>>> 732731e2ebe6e4f8a01f855637362f4172ca8e92
     }
 }
