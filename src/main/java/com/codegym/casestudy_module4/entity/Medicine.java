f58c380865aa38937f8963e6f4706ff19ca798e6
Dang Minh Nhat <dgminhnhat1991@gmail.com>
2025-03-14 13:26:44 +0700

    sửa lại chỗ update do mất trường create_at của employee

--- src/main/java/com/codegym/casestudy_module4/entity/Medicine.java
+++ src/main/java/com/codegym/casestudy_module4/entity/Medicine.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
